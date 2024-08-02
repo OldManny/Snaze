b79185326e555774fd3c990dec2b17a3323c429b
Emanuel Florea <floreae@roehampton.ac.uk>
2024-08-02 17:22:27 +0100

    Styled S icon to use as Home in the navbar

---
+++ components/SIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Render a styled "S" icon
+const SIcon = ({ className }) => (
+  <div className={`s-icon ${className}`}>
+    S {/* Display the letter "S" */}
+  </div>
+);
+
+export default SIcon;
